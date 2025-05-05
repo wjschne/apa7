b796df6f88ba1995ec1a4e4b3d0277092d2a7a50
W. Joel Schneider <w.joel.schneider@gmail.com>
2025-05-05 06:59:54 -0400

    document p value

--- R/apaquarto.R
+++ R/apaquarto.R
@@ -44,32 +44,36 @@ install_apaquarto <- function(make_project = TRUE) {
 #' p-value in APA format
 #'
 #' @param p probability
-#' @param inline If TRUE (default), returns statistic (e.g.,e p = .04), otherwise just the number (e.g., .04)
+#' @param inline If TRUE (default), returns statistic (e.g., p = .04), otherwise just the number (e.g., .04)
 #'
 #' @return character vector
 #' @export
 #'
 #' @examples
 #' # Values less than .001 are <.001
-#' apa_p(.0002)
+#' apa_p(.0002, inline = FALSE)
 #' # Values between .001 and .01 are rounded to 3 digits
-#' apa_p(.002)
+#' apa_p(.002, inline = FALSE)
 #' # Values between .01 and .995 are rounded to 2 digits
-#' apa_p(.02)#'
-#' apa_p(.22)
-#' apa_p(.994)
+#' apa_p(.02, inline = FALSE)
+#' apa_p(.22, inline = FALSE)
+#' apa_p(.994, inline = FALSE)
 #' # Values above .995 are >.99
-#' apa_p(.999)
+#' apa_p(.999, inline = FALSE)
 apa_p <- function(p,
                   inline = TRUE) {
 
-  if (p < 0.001 && inline) return("*p*&nbsp;<&nbsp;.001")
-  if (p < 0.001 && !inline) return("<.001")
   digit <- ifelse(p < .0095, 3, 2)
   p_formatted <- sapply(p, function(x) formatC(round(x, digit + 1), digit, format = "f"))
+
   p_formatted <- sub(x = p_formatted ,
                      pattern = "^0\\.",
                      replacement = ".")
-  paste0(ifelse(inline, "*p*&nbsp;=&nbsp;", ""), p_formatted)
+  p_formatted <- paste0(ifelse(inline, "*p*&nbsp;=&nbsp;", ""), p_formatted)
+  p_formatted[p < 0.001 & inline] <- "*p*&nbsp;<&nbsp;.001"
+  p_formatted[p < 0.001 & !inline] <- "<.001"
+  p_formatted[p >= 0.995 & inline] <- "*p*&nbsp;>&nbsp;.99"
+  p_formatted[p >= 0.995 & !inline] <- ">.99"
+  p_formatted
 }
 
